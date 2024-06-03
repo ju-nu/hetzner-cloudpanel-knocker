f34f02b92d55b805f3bd291a9266c81afe873573
Sebastian Gräbner <djcrackhome@icloud.com>
2024-06-03 12:47:58 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,9 +7,9 @@ NEW_IP="$1/32"  # Use the passed IP address
 # Fetch the current firewall details
 firewall_details=$(curl -s -H "Authorization: Bearer $API_TOKEN" "https://api.hetzner.cloud/v1/firewalls/$FIREWALL_ID")
 
-# Extract existing rules, remove the rule with the specified port
+# Extract existing rules, remove the rule with the specified port 8443
 existing_rules=$(echo "$firewall_details" | jq '.firewall.rules')
-filtered_rules=$(echo "$existing_rules" | jq 'map(select(.port != "'"$PORT"'" and .protocol != "tcp"))')
+filtered_rules=$(echo "$existing_rules" | jq 'map(select(.port != "8443" and .port != "22" and .protocol != "tcp"))')
 
 # Prepare the data for the PUT request to remove the old rule
 update_data=$(jq -n --argjson rules "$filtered_rules" '{rules: $rules}')
@@ -18,13 +18,13 @@ update_data=$(jq -n --argjson rules "$filtered_rules" '{rules: $rules}')
 curl -X PUT -H "Authorization: Bearer $API_TOKEN" -H "Content-Type: application/json" -d "$update_data" "https://api.hetzner.cloud/v1/firewalls/$FIREWALL_ID"
 
 # Create the new rule with the updated IP address for port 8443
-new_rule_8443=$(jq -n --arg port "$PORT" --arg ip "$NEW_IP" '{"direction": "in", "protocol": "tcp", "port": $port, "source_ips": [$ip], "destination_ips": []}')
+new_rule_8443=$(jq -n --arg port "8443" --arg ip "$NEW_IP" '{"direction": "in", "protocol": "tcp", "port": $port, "source_ips": [$ip], "destination_ips": []}')
 
 # Create rules for all IPv4 and IPv6 on ports 80 and 443
 rules_ports_80_443=()
-for port in $(echo "${PORTS//,/ }"); do
-    rule_ipv4=$(jq -n --arg port "$port" --arg ip "$IPV4" '{"direction": "in", "protocol": "tcp", "port": $port, "source_ips": [$ip], "destination_ips": []}')
-    rule_ipv6=$(jq -n --arg port "$port" --arg ip "$IPV6" '{"direction": "in", "protocol": "tcp", "port": $port, "source_ips": [$ip], "destination_ips": []}')
+for port in 80 443; do
+    rule_ipv4=$(jq -n --arg port "$port" --arg ip "0.0.0.0/0" '{"direction": "in", "protocol": "tcp", "port": $port, "source_ips": [$ip], "destination_ips": []}')
+    rule_ipv6=$(jq -n --arg port "$port" --arg ip "::/0" '{"direction": "in", "protocol": "tcp", "port": $port, "source_ips": [$ip], "destination_ips": []}')
     rules_ports_80_443+=("$rule_ipv4")
     rules_ports_80_443+=("$rule_ipv6")
 done
