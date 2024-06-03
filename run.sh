e2ad0beb3b1acc76e6ba90168df27923c0aac1e8
Sebastian Gräbner <djcrackhome@icloud.com>
2024-06-03 13:28:20 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ NEW_IP="$1/32"  # Use the passed IP address
 # Fetch the current firewall details
 firewall_details=$(curl -s -H "Authorization: Bearer $API_TOKEN" "https://api.hetzner.cloud/v1/firewalls/$FIREWALL_ID")
 
-# Extract existing rules, remove the rule with the specified port 8443
+# Extract existing rules, remove the rule with the specified port 8443 and 22
 existing_rules=$(echo "$firewall_details" | jq '.firewall.rules')
 filtered_rules=$(echo "$existing_rules" | jq 'map(select(.port != "8443" and .port != "22" and .protocol != "tcp"))')
 
@@ -20,20 +20,25 @@ curl -X PUT -H "Authorization: Bearer $API_TOKEN" -H "Content-Type: application/
 # Create the new rule with the updated IP address for port 8443
 new_rule_8443=$(jq -n --arg port "8443" --arg ip "$NEW_IP" '{"direction": "in", "protocol": "tcp", "port": $port, "source_ips": [$ip], "destination_ips": []}')
 
-# Create rules for all IPv4 and IPv6 on ports 80 and 443
-rules_ports_80_443=()
-for port in 80 443; do
-    rule_ipv4=$(jq -n --arg port "$port" --arg ip "0.0.0.0/0" '{"direction": "in", "protocol": "tcp", "port": $port, "source_ips": [$ip], "destination_ips": []}')
-    rule_ipv6=$(jq -n --arg port "$port" --arg ip "::/0" '{"direction": "in", "protocol": "tcp", "port": $port, "source_ips": [$ip], "destination_ips": []}')
-    rules_ports_80_443+=("$rule_ipv4")
-    rules_ports_80_443+=("$rule_ipv6")
-done
-
 # Create the new rule for port 22 with the updated IP address
 new_rule_22=$(jq -n --arg port "22" --arg ip "$NEW_IP" '{"direction": "in", "protocol": "tcp", "port": $port, "source_ips": [$ip], "destination_ips": []}')
 
+# Create rules for all IPv4 and IPv6 on ports 80 and 443
+rules_ports_80_443=()
+for port in 80 443; do
+    rule=$(jq -n --arg port "$port" --arg ip4 "0.0.0.0/0" --arg ip6 "::/0" '{"direction": "in", "protocol": "tcp", "port": $port, "source_ips": [$ip4, $ip6], "destination_ips": []}')
+    rules_ports_80_443+=("$rule")
+done
+
+# Create ICMP rules for all IPv4 and IPv6
+rules_icmp=()
+for protocol in icmp; do
+    rule=$(jq -n --arg protocol "$protocol" --arg ip4 "0.0.0.0/0" --arg ip6 "::/0" '{"direction": "in", "protocol": $protocol, "source_ips": [$ip4, $ip6], "destination_ips": []}')
+    rules_icmp+=("$rule")
+done
+
 # Combine all the new rules
-all_new_rules=$(jq -n --argjson rule1 "$new_rule_8443" --argjson rule2 "$new_rule_22" --argjson rule3 "$(echo "${rules_ports_80_443[@]}" | jq -s '.')" '$rule3 + [$rule1, $rule2]')
+all_new_rules=$(jq -n --argjson rule1 "$new_rule_8443" --argjson rule2 "$new_rule_22" --argjson rule3 "$(echo "${rules_ports_80_443[@]}" | jq -s '.')" --argjson rule4 "$(echo "${rules_icmp[@]}" | jq -s '.')" '$rule3 + [$rule1, $rule2] + $rule4')
 
 # Prepare the final data for setting the new rules
 final_data=$(jq -n --argjson rules "$all_new_rules" '{rules: $rules}')
